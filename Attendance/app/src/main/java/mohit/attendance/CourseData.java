599ad0f08d4053d9da8724593614b19dc92c148c
ByteLandian <mohitg@iitrpr.ac.in>
2015-04-25 03:16:02 +0530

    Init Android Project

---
+++ Attendance/app/src/main/java/mohit/attendance/CourseData.java
@@ -0,0 +1,33 @@
+package mohit.attendance;
+
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by mohit on 25/4/15.
+ */
+public class CourseData {
+
+    String course_code;
+    int semester,year;
+    JSONArray teaches;
+
+    CourseData(JSONObject j)
+    {
+        Log.d("Attendance", "Adding Data");
+        try {
+            course_code = j.getString("course_id");
+            year = j.getInt("year");
+            semester = j.getInt("semester");
+            teaches = (JSONArray) j.get("teaching");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ Attendance/app/src/main/java/mohit/attendance/EfficientAdapter.java
@@ -0,0 +1,71 @@
+package mohit.attendance;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mohit on 25/4/15.
+ */
+public class EfficientAdapter extends BaseAdapter {
+
+    static ArrayList<CourseData> data=new ArrayList<CourseData>();
+
+    private LayoutInflater mInflater;
+
+    public EfficientAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        Log.d("Attendance", "Called");
+        super.notifyDataSetChanged();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, null);
+
+
+            holder = new ViewHolder();
+
+            holder.title = (TextView) convertView.findViewById(R.id.title);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.title.setText(data.get(position).course_code);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView title;
+    }
+}

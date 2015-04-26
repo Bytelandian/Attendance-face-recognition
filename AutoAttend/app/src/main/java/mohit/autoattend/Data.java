ea5e865c6dca267344ec8ac289ab006f5dbcdf14
ByteLandian <mohitg@iitrpr.ac.in>
2015-04-26 19:29:24 +0530

    Java File containing the data of euclidean distance and ID

---
+++ AutoAttend/app/src/main/java/mohit/autoattend/Data.java
@@ -0,0 +1,25 @@
+package mohit.autoattend;
+
+import java.util.Comparator;
+
+/**
+ * Created by mohit on 26/4/15.
+ */
+public class Data
+{
+    public double dist;
+    public int id;
+    public Data(double dist, int id) {
+        this.dist = dist;
+        this.id = id;
+    }
+}
+
+class Distance implements Comparator<Data> {
+
+    public int compare(Data d, Data t) {
+        // usually toString should not be used,
+        // instead one of the attributes or more in a comparator chain
+        return (t.dist < d.dist) ? 1 : -1;
+    }
+}
